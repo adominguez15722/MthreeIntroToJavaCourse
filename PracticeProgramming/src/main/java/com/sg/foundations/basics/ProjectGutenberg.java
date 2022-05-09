51f0a07c64da73b649dec299d0a41a9bebc1ca3f
Anthony <adominguez15722@gmail.com>
2022-05-09 12:50:16 -0500

    Differences between print and println

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/ProjectGutenberg.java
@@ -0,0 +1,18 @@
+
+package com.sg.foundations.basics;
+
+/**
+ *
+ * @author Anthony Dominguez <Anthony.Dominguez at adominguez15722@gmail.com>
+ */
+public class ProjectGutenberg {
+    public static void main(String[] args) {
+        System.out.print("Did you know that in 1440 (or thereabouts),");
+        System.out.println("Johannes Gutenberg invented the printing press?");
+        System.out.println("He started out as a goldsmith!");
+        System.out.print("His invention made it easy to print and");
+        System.out.println("distribute books to anyone who wanted one.");
+        System.out.print("We are like a modern Gutenberg,");
+        System.out.print("printing vast amounts to the waiting console with ease.");
+    }
+}
