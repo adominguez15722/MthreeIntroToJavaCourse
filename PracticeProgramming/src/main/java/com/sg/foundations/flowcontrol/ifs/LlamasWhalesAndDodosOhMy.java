d84885b9f0b5eda3c3428b3eda3f0083397ef4bf
Anthony <adominguez15722@gmail.com>
2022-05-10 17:05:42 -0500

    working on if statements exercise

--- PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/ifs/LlamasWhalesAndDodosOhMy.java
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/ifs/LlamasWhalesAndDodosOhMy.java
@@ -15,26 +15,20 @@ public class LlamasWhalesAndDodosOhMy {
         if (dodos > 0) {
             System.out.println("Egads, I thought dodos were extinct!");
         }
-
         if(dodos < 0){
             System.out.println("Hold on, how can we have NEGATIVE dodos??!");
         }
-
         if(llamas > whales){
             System.out.println("Whales may be bigger, but llamas are better, ha!");
         }
-
         if(llamas <= whales){
             System.out.println("Aw man, brawn over brains I guess. Whales beat llamas.");
         }
-
         System.out.println("There's been a huge increase in the dodo population via cloning!");
         dodos += 100;
-
         if( (whales + llamas) < dodos){
             System.out.println("I never thought I'd see the day when dodos ruled the earth.");
         }
-
         if(llamas > whales && llamas > dodos){
             System.out.println("I don't know how, but the llamas have come out ahead! Sneaky!");
         }
