133e7ebfd11bc752a6f5c9dcaf00ba33c2980b53
Anthony <adominguez15722@gmail.com>
2022-05-11 13:55:58 -0500

    Array exercises

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/arrays/ARainbow.java
@@ -0,0 +1,16 @@
+
+package com.sg.foundations.flowcontrol.arrays;
+
+/**
+ *
+ * @author Anthony Dominguez <Anthony.Dominguez at adominguez15722@gmail.com>
+ */
+public class ARainbow {
+    public static void main(String[] args) {
+        String[] colors = {"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"};
+        
+        for (int i=0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+    }
+}
