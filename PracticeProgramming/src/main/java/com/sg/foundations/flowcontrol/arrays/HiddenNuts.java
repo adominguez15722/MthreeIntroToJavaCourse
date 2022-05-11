13641d8d211d475ed8ec7f2d58612e991dac0260
Anthony <adominguez15722@gmail.com>
2022-05-11 14:50:03 -0500

    Array exercises complete

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/arrays/HiddenNuts.java
@@ -0,0 +1,33 @@
+
+package com.sg.foundations.flowcontrol.arrays;
+
+/**
+ *
+ * @author Anthony Dominguez <Anthony.Dominguez at adominguez15722@gmail.com>
+ */
+import java.util.Random;
+
+public class HiddenNuts {
+    public static void main(String[] args) {
+        String[] hidingSpots = new String[100];
+        System.out.println("The nut has been hidden ...");
+        Random squirrel = new Random();
+        int upperbound = hidingSpots.length;
+        hidingSpots[squirrel.nextInt(upperbound)] = "Nut";
+        for (int i = 0; i < hidingSpots.length; i++) {
+            if(hidingSpots[i] != null) {
+                System.out.println("Found it! It's in spot#" + i);
+            }
+        }
+    }
+//    static void findNut(String arr[]) {
+//        Random squirrel = new Random();
+//        arr[squirrel.nextInt(arr.length)] = "Nut";
+//        for (int i = 0; i < arr.length; i++) {
+//            if(arr[i].equals("Nut")) {
+//                System.out.println("Found it! It's in spot#" + i);
+//            }
+//        }
+//    
+//    }
+}
