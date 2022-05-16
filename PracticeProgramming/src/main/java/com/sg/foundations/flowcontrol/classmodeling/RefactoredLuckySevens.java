23813012691e0ec017d830d80e490b165399cc87
Anthony <adominguez15722@gmail.com>
2022-05-16 10:13:26 -0500

    lucky sevens refactored

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/classmodeling/RefactoredLuckySevens.java
@@ -0,0 +1,48 @@
+
+package com.sg.foundations.flowcontrol.classmodeling;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Anthony Dominguez <Anthony.Dominguez at adominguez15722@gmail.com>
+ */
+public class RefactoredLuckySevens {
+    public static void main(String[] args) {
+        LuckySevens play = new LuckySevens();
+        play.sevens();
+    }
+}
+
+
+
+
+
+class LuckySevens {
+    public void sevens(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many dollars do you have?");
+        int amount = scanner.nextInt();
+        int count = 0;
+        int profit = amount;
+        int profitCount = 0;
+        double sum = 0;
+        while (amount > 0) {
+            double rollOne = Math.round(Math.random() * (6 - 1) + 1);
+            double rollTwo = Math.round(Math.random() * (6 - 1) + 1);
+            if(rollOne + rollTwo == 7) {
+                if (profit <= amount) {
+                    profitCount = count;
+                    profit +=4;
+                }
+                amount += 4;
+                count +=1;
+            } else {
+                amount -= 1;
+                count +=1;
+            }
+        }
+        System.out.println("You are broke after " + count + " rolls.");
+        System.out.println("You should have quit after " + profitCount + " rolls when you had $" + profit);
+    }
+}
