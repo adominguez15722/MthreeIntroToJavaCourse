e79495467099f69ed0e26de5965987331482fa3b
Anthony <adominguez15722@gmail.com>
2022-05-16 09:27:51 -0500

    refactored rock paper scissors

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/classmodeling/ClassModeling.java
@@ -0,0 +1,14 @@
+
+package com.sg.foundations.flowcontrol.classmodeling;
+
+/**
+ *
+ * @author Anthony Dominguez <Anthony.Dominguez at adominguez15722@gmail.com>
+ */
+public class ClassModeling {
+        private String address;
+        
+        public String rooms(String roomName) {
+            
+        }
+}
