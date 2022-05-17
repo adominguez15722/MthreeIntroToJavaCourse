5e2477ae405f746fd0dc42c9e16d7a5e6feed66a
Anthony <adominguez15722@gmail.com>
2022-05-17 09:09:35 -0500

    class inheritance exercises

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/shapesperimeters/ShapesAndPerimeters.java
@@ -0,0 +1,17 @@
+
+package com.sg.foundations.flowcontrol.shapesperimeters;
+
+/**
+ *
+ * @author Anthony Dominguez <Anthony.Dominguez at adominguez15722@gmail.com>
+ */
+public class ShapesAndPerimeters {
+    private String color;
+    
+    public float getArea() {
+        float area = ;
+        return area;
+    };
+    
+    
+}
