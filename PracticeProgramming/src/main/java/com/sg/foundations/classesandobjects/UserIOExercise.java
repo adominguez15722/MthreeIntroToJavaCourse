e849ce911f847b23c29766d7e8f565926ee8bbc4
Anthony <adominguez15722@gmail.com>
2022-05-18 11:40:43 -0500

    UserIO exercise

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/classesandobjects/UserIOExercise.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.classesandobjects;
+
+/**
+ *
+ * @author Anthony Dominguez <Anthony.Dominguez at adominguez15722@gmail.com>
+ */
+public class UserIOExercise {
+    
+}
